e34897b01c1427802727612f5813d4bb0b9de3b4
abalmasDA <abalmasit@gmail.com>
2023-07-09 19:56:26 +0300

    Commit1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Main {
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * Мінімальне з N чисел (використовувати LinkedList):
+ * 1. Введіть із клавіатури число N.
+ * 2. Вважати N цілих чисел і заповнити ними список - метод getIntegerList.
+ * 3. Знайти мінімальне число серед елементів списку – метод getMinimum.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scannerInputNumber = new Scanner(System.in);
+        try {
+            System.out.print("You need to input number (only integer type) of elements of the list: ");
+            int numberList = scannerInputNumber.nextInt();
+            LinkedList<Integer> someList = NumberManipulation.getIntegerList(numberList);
+            System.out.println("Some list integer numbers:" + someList);
+            System.out.println("Minimum number is: " + NumberManipulation.getMinimum(someList));
+
+        } catch (InputMismatchException exception) {
+            System.out.println("You entered the wrong number format ");
+        }
+
+
+    }
+
+
 }
--- src/NumberManipulation.java
+++ src/NumberManipulation.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class NumberManipulation {
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class NumberManipulation {
+
+    public static LinkedList<Integer> getIntegerList(int numberElementsOfList) {
+        LinkedList<Integer> resultList = new LinkedList<>();
+        for (int i = 0; i < numberElementsOfList; i++) {
+            resultList.add(((int) (Math.random() * (1000 + 1)) - 100));
+        }
+        return resultList;
+    }
+
+    public static int getMinimum(LinkedList<Integer> list) {
+        Collections.sort(list);
+        return list.getFirst();
+    }
+
+
 }
